f6a8caef299e93e48373f5aa295fdd1d24a8de71
SvetlanaKobushko <svetlana.kobushko@nu.edu.kz>
2022-11-14 23:00:50 +0600

    some more sweat and tears

--- main.js
+++ main.js
@@ -27,32 +27,32 @@ document.body.appendChild(p1);
 var ul1 = document.createElement("ul");
 
 var li1 = document.createElement("li");
-li1.innerHTML = "find elements in the DOM <strong>(5 points)</strong>;";
+li1.innerHTML = "find elements in the DOM (<strong>5 points</strong>);";
 li1.classList.add("odd");
 ul1.appendChild(li1);
 
 var li2 = document.createElement("li");
-li2.innerHTML = "create/add/remove elements <strong>(5 points)</strong>;";
+li2.innerHTML = "create/add/remove elements (<strong>5 points</strong>);";
 li2.classList.add("even");
 ul1.appendChild(li2);
 
 var li3 = document.createElement("li");
-li3.innerHTML = "change content of the elements <strong>(5 points)</strong>;";
+li3.innerHTML = "change content of the elements (<strong>5 points</strong>);";
 ul1.appendChild(li3);
 li3.classList.add("odd");
 
 var li4 = document.createElement("li");
-li4.innerHTML = "change styles of the elements <strong>(5 points)</strong>;";
+li4.innerHTML = "change styles of the elements (<strong>5 points</strong>);";
 li4.classList.add("even");
 ul1.appendChild(li4);
 
 var li5 = document.createElement("li");
-li5.innerHTML = "change attributes of the elements <strong>(5 points)</strong>;";
+li5.innerHTML = "change attributes of the elements (<strong>5 points</strong>);";
 ul1.appendChild(li5);
 li5.classList.add("odd");
 
 var li6 = document.createElement("li");
-li6.innerHTML = "change classes of the elements <strong>(5 points)</strong>.";
+li6.innerHTML = "change classes of the elements (<strong>5 points</strong>).";
 li6.classList.add("even");
 ul1.appendChild(li6);
 
@@ -66,7 +66,7 @@ p2.innerHTML = "Basic necessary code can be found in the supplementary materials
 
 let a1 = document.createElement("a");
 a1.setAttribute("href", "https://github.com/yessen/nu_web_programming/tree/main/week12/lab12");
-a1.innerText = "link";
+a1.innerText = "link.";
 p2.appendChild(a1);
 document.body.appendChild(p2);
 
@@ -91,7 +91,7 @@ document.body.appendChild(p3);
 var ul2 = document.createElement("ul");
 
 var li11 = document.createElement("li");
-li11.innerHTML = "Create a new repository on Github, named <strong>lab12 (1 point)</strong>.";
+li11.innerHTML = "Create a new repository on Github, named <strong>lab12 </strong>(<strong>1 point</strong>).";
 ul2.appendChild(li11);
 li11.classList.add("odd");
 
@@ -101,13 +101,13 @@ li12.classList.add("even");
 ul2.appendChild(li12);
 
 var li13 = document.createElement("li");
-li13.innerHTML = "Create a new HTML file, called <strong>index.html</strong>, which has only one &lth1&gt tag with \"Hello, World!\" message <strong>(1 point)</strong>.";
+li13.innerHTML = "Create a new HTML file, called <strong>index.html</strong>, which has only one &lth1&gt tag with \"Hello, World!\" message (<strong>1 point</strong>).";
 ul2.appendChild(li13);
 li13.classList.add("odd");
 
 
 var li14 = document.createElement("li");
-li14.innerHTML = "Create a new JavaScript file, called <strong>main.js</strong>, which must contain your program (assignment) described above <strong>(1 point)</strong>.";
+li14.innerHTML = "Create a new JavaScript file, called <strong>main.js</strong>, which must contain your program (assignment) described above (<strong>1 point</strong>).";
 li14.classList.add("even");
 ul2.appendChild(li14);
 
@@ -117,12 +117,12 @@ ul2.appendChild(li15);
 li15.classList.add("odd");
 
 var li16 = document.createElement("li");
-li16.innerHTML = "Write a JavaScript program in <strong>main.js</strong> to make your <strong>index.html</strong> look identical to this HTML file <strong>(5 points)</strong>.";
+li16.innerHTML = "Write a JavaScript program in <strong>main.js</strong> to make your <strong>index.html</strong> look identical to this HTML file (<strong>5 points</strong>).";
 li16.classList.add("even");
 ul2.appendChild(li16);
 
 var li17 = document.createElement("li");
-li17.innerHTML = "After you finish your work, submit it to the Github <strong>(2 points)</strong>.";
+li17.innerHTML = "After you finish your work, submit it to the Github (<strong>2 points</strong>).";
 ul2.appendChild(li17);
 li17.classList.add("odd");
 
